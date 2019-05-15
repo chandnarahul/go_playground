24835bc9b9040d837fdef10a6f533382ad0b1241
Rahul Chandna <chandnarahul@yahoo.com>
2019-05-15 22:40:36 +0200

    user input

---
+++ HelloWorldInput.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Hello World To: ")
+	fmt.Println("---------------------")
+
+	for {
+		fmt.Print("-> ")
+		text, _ := reader.ReadString('\n')
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		fmt.Println(text)
+	}
+
+}
