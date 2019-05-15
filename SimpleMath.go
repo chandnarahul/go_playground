e472934f016d0359f3c11cfb543ff3870365d5a5
Rahul Chandna <chandnarahul@yahoo.com>
2019-05-15 22:52:49 +0200

    added new lines

--- SimpleMath.go
+++ SimpleMath.go
@@ -7,8 +7,8 @@ import (
 func main() {
 	first := 10
 	second := 20
-	fmt.Printf("%d + %d = %d", first, second, (first + second))
-	fmt.Printf("%d - %d = %d", first, second, (first - second))
-	fmt.Printf("%d * %d = %d", first, second, (first * second))
-	fmt.Printf("%d / %d = %d", first, second, (first / second))
+	fmt.Printf("%d + %d = %d\n", first, second, (first + second))
+	fmt.Printf("%d - %d = %d\n", first, second, (first - second))
+	fmt.Printf("%d * %d = %d\n", first, second, (first * second))
+	fmt.Printf("%d / %d = %d\n", first, second, (first / second))
 }
